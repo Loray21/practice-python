7703545fa8bd87f1f2c401e9dd53a47f591b776c
Tomas Loray <tomas.loray@globant.com>
2025-11-10 09:00:43 -0300

    finish practice current

--- concurrent/script.py
+++ concurrent/script.py
@@ -9,30 +9,6 @@ BASE_URL = "https://graph.facebook.com/v22.0"
 HEADERS = {"Authorization": f"Bearer {ACCESS_TOKEN}"}
 
 
-def task_call(i):
-    try:
-        accounts= get_adaccounts()
-        print(f"[{i}] llamada ok - {len(accounts)} cuents")
-        return {"i": i , "status": True, "count": len(accounts)}
-    except Exception as e:
-        return {"i": i,"status":False, "err": str(e)}
-        
-
-def run_many_call(total_calls=5000, max_workers=50):
-    results=[]
-    with ThreadPoolExecutor(max_workers=max_workers) as ex:
-        ##Esto es una list comprehension
-        futures = [ex.submit(task_call,i) for i in  range(1, total_calls + 1)]
-        for fut in as_completed(futures):
-            results.append(fut.result())
-
-    
-    return results
-
-
-    
-
-
 def fetch(endpoint, params=None):
     url = f"{BASE_URL}/{endpoint}"
     if params is None:
@@ -48,19 +24,41 @@ def fetch(endpoint, params=None):
         print(f"Error calling {endpoint}: {e}")
         return None
 
+
 def get_adaccounts():
     data = fetch("me/adaccounts", {"fields": "id,name,created_time"})
     return data.get("data", []) if data else []
 
-accounts = get_adaccounts()
-retries = 0 
-while retries <= 5000:
-    accounts = get_adaccounts()
-    print("una llamda")
-    retries +=1
 
-    for acc in accounts:
-        print(f"Cuenta: {acc['name']} ({acc['id']})")
+def task_call(i):
+    try:
+        accounts = get_adaccounts()
+        print(f"[{i}] llamada ok - {len(accounts)} cuents")
+        return {"i": i, "status": True, "count": len(accounts)}
+    except Exception as e:
+        return {"i": i, "status": False, "err": str(e)}
+        
+
+def run_many_call(total_calls=50000, max_workers=100):
+    results = []
+    with ThreadPoolExecutor(max_workers=max_workers) as ex:
+        futures = [ex.submit(task_call, i) for i in range(1, total_calls + 1)]
+        for fut in as_completed(futures):
+            results.append(fut.result())    
+    return results
+
+
+result = run_many_call()
+print(result)
+# accounts = get_adaccounts()
+# retries = 0 
+# while retries <= 5000:
+#     accounts = get_adaccounts()
+#     print("una llamda")
+#     retries +=1
+
+#     for acc in accounts:
+#         print(f"Cuenta: {acc['name']} ({acc['id']})")
 
 
         
